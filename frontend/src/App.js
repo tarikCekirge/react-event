2ec46ec0eb72339af6a8f5081987f3779b3f766c
Tarık Çekirge <tcekirge@gmail.com>
2025-03-18 18:43:25 +0300

    Dynamic Routes & loaders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./pages/Root";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRoot from "./pages/EventsRoot";
@@ -24,7 +26,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ":eventId", element: <EventDetailPage /> },
+          {
+            path: ":eventId",
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const { eventId } = useParams();
-  return (
-    <>
-      <h1>EventDetailPage</h1>
-      <p> EVent id: {eventId} </p>
-    </>
-  );
+  const data = useLoaderData();
+
+  return <EventItem event={data.event} />;
 };
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not fetch details for seleted event" }),
+      {
+        status: 500,
+        statusText: "Could not fetch events",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } else {
+    return response;
+  }
+}
